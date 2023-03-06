f0dd43944b89787905c96b1d839c431ad56e5553
Bozena11 <bozena.vuckovic@gmail.com>
2023-03-06 21:55:27 +0100

    Conditional status

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,13 +1,24 @@
+/*if we just put status: string any string can be passed from App.tsx
+with condition App.tsx will warn us that it can accept only the 3 options listed
+ so if in App.tsx I write 'status='test' it will complain*/
+
 type StatusProps = {
-	status: string
+	/*status: string*/
+	status: 'loading' | 'success' | 'error'
 }
 
 export const Status = (props: StatusProps) => {
+	let message;
+	if (props.status === 'loading') {
+		message = 'Loading...'
+	} else if (props.status === 'success') {
+		message = 'Data fetched successfully!'
+	} else if ( props.status === 'error') {
+		message = 'Error fetching data'
+	}
 	return (
 		<div>
-			<h2>Loading...</h2>
-			<h2>Data fetched successfully!</h2>
-			<h2>Error fetching data</h2>
+			<h2>Status - {message}</h2>
 		</div>
 	)
 }
