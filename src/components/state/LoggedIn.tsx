78e53c020fd62ab07bfd48680a563f20a2c99994
Bozena11 <bozena.vuckovic@gmail.com>
2023-03-07 11:08:25 +0100

    Declare initial state, Typescript recognizes automatically

--- src/components/state/LoggedIn.tsx
+++ src/components/state/LoggedIn.tsx
@@ -4,13 +4,17 @@ export const LoggedIn = () => {
 
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const handleLogin = () => {}
-	const handleLogout = () => {}
+	const handleLogin = () => {
+		setIsLoggedIn(true)
+	}
+	const handleLogout = () => {
+		setIsLoggedIn(false)
+	}
 	return (
 		<div>
 			<button onClick={handleLogin}>Login</button>
 			<button onClick={handleLogout}>Logout</button>
-			<div>User is logged in/logged out</div>
+			<div>User is {isLoggedIn ? 'logged in' : 'logged out'}</div>
 		</div>
 	)
 }
