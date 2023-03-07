5bbfae01da17a3dc26792d2c3c311c3fd676b892
Bozena11 <bozena.vuckovic@gmail.com>
2023-03-07 10:22:58 +0100

    Destructure props and separate file for types

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,12 @@
-/* type for object */
-type PersonProps = {
+/* type for object - moved to Person.types.ts */
+/*type PersonProps = {
 	name: {
 		first: string
 		last: string
 	}
-}
+}*/
+
+import { PersonProps } from "./Person.types"
 
 export const Person = (props: PersonProps) => {
 	return (
