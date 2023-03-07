079a4881631f23eeb59bb110b8c051511e478863
Bozena11 <bozena.vuckovic@gmail.com>
2023-03-07 13:23:10 +0100

    UseReducer Hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Counter } from './components/state/Counter';
 import { Status } from './components/Status';
 
 
@@ -48,6 +49,7 @@ function App() {
       />
       <Input value='' handleChange={(event) => console.log(event)} />
       <Container styles={{ border: '1px solid black', padding: '1rem' }} />
+      <Counter />
     </div>
   );
 }
