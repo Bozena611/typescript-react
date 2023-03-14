c74f2aca5098ae134d888fe63ca40c2439af82c0
Bozena11 <bozena.vuckovic@gmail.com>
2023-03-14 11:22:33 +0100

    UseContext Future Value

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Counter } from './components/state/Counter';
 import { Status } from './components/Status';
+import { UserContextProvider } from './components/context/UserContext';
+import { User } from './components/context/User';
 
 
 function App() {
@@ -55,6 +57,9 @@ function App() {
       <ThemeContextProvider>
         <Box />
       </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
